2b6ad1550bfd8d603c8bedb80c820a7712b68094
Artur <artur.matkowski.zan@gmail.com>
2020-11-10 18:55:17 +0100

    removed glut & freeglut from PCH

--- inc/PrecompiledHeaders.hpp
+++ inc/PrecompiledHeaders.hpp
@@ -1,26 +1,8 @@
 #ifndef _H_PRECOMPLIED_HEADERS
 #define _H_PRECOMPLIED_HEADERS
 
-
-
-#define GLEW_OSMESA
-
-
-
-//#include <GL/glu_mangle.h>
-//#include <GL/glx_mangle.h>
-
-//#include <GL/osmesa.h>
-//#include <GLES2/gl2.h>
-
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-
-
-#include <GL/freeglut.h>
-
-
-#include <GL/glut.h>
+//#include <GL/freeglut.h>
+//#include <GL/glut.h>
 
 
 
