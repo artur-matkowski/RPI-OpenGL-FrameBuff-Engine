c1fa1c51b2cfb87c4922bce136f088c9194aae86
artur <artur@debian.home>
2021-05-28 22:15:57 +0200

    configure.sh yweaks

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt install libgbm-dev libgles2-mesa-dev nvidia-driver-libs libassimp-dev assimp-utils
+apt install g++ libgbm-dev libgles2-mesa-dev nvidia-driver-libs libassimp-dev assimp-utils libpng-dev libglfw3 libglfw3-dev
